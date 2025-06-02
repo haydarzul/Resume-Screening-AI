91d4b602a3c0721e6d66bec0f262cbef2455ed03
NadiaQotrunadaa <qotrunadanadia@gmail.com>
2025-06-02 16:13:12 +0700

    Updated AI-Resume-Screening

--- main.py
+++ main.py
@@ -84,4 +84,4 @@ def predict():
         return jsonify({'error': 'File type not allowed'}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=81)
